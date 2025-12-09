299e51513079aa3d7475ffc04f16b830f441de0a
Guilherme Serafin de Carvalho <98965845+guiserafin@users.noreply.github.com>
2025-12-09 20:02:45 +0000

    fix: update Angular CLI version and refactor item-certificado component
    - Updated @angular/cli from version 20.3.2 to 20.3.13 in package.json.
    - Changed the button in item-certificado.html to use a click event for navigation instead of routerLink.
    - Implemented redirecionaCertificado method in item-certificado.ts to handle navigation programmatically using Router.

--- src/app/_components/item-certificado/item-certificado.ts
+++ src/app/_components/item-certificado/item-certificado.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { SecundaryButton } from '../secundary-button/secundary-button';
-import { RouterLink } from "@angular/router";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-item-certificado',
-  imports: [SecundaryButton, RouterLink],
+  imports: [SecundaryButton],
   templateUrl: './item-certificado.html',
   styleUrl: './item-certificado.css'
 })
 export class ItemCertificado {
   id: string = '6';
+
+  constructor(private router: Router) {}
+
+  redirecionaCertificado() {
+    this.router.navigate(['/certificados', this.id]);
+    // this.router.navigateByUrl(`/certificados/${this.id}`); <-- outra forma de fazer, mas a de cima é uma boa prática
+  }
 }

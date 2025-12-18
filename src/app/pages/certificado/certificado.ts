5a1f6cad0c715a7eb60cf4bda55ecc7e68f190b3
Guilherme Serafin de Carvalho <98965845+guiserafin@users.noreply.github.com>
2025-12-18 17:08:53 +0000

    feat: Adicionar funcionalidade de download de certificado com html2canvas

--- src/app/pages/certificado/certificado.ts
+++ src/app/pages/certificado/certificado.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { SecundaryButton } from '../../_components/secundary-button/secundary-button';
 import { ActivatedRoute, RouterLink } from "@angular/router";
 import { CertificadoService } from '../../_services/certificado.service';
 import {OnInit} from '@angular/core';
 import { Certificado as ICertificado} from '../../interfaces/certificado';
+import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-certificado',
   imports: [SecundaryButton, RouterLink],
@@ -15,6 +16,8 @@ export class Certificado implements OnInit {
   id: string | null = null;
   certificado: ICertificado | undefined;
 
+  @ViewChild('certificadoContainer') certificadoElement!: ElementRef;
+
   constructor(private certificadoService: CertificadoService, private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -24,5 +27,19 @@ export class Certificado implements OnInit {
       this.certificado = this.certificadoService.certificados.find(cert => cert.id === this.id);
     });
 
+  }
+
+  downloadCertificado() {
+
+    if (this.certificado ==  undefined) {
+      return;
+    }
+
+    html2canvas(this.certificadoElement.nativeElement, {scale: 2}).then(canvas => {
+      const link = document.createElement('a');
+      link.download = `certificado_${this.certificado?.nome.replaceAll(" ", "_")}.png`;
+      link.href = canvas.toDataURL('image/png');
+      link.click();
+    });
   }
 }
